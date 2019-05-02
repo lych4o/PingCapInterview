ab208343e3349f1f06cff3faed70d2d80ccd9c74
lych <lych@lychdeMacBook-Air.local>
2019-05-02 12:14:03 +0800

    init thread_pool

--- mapreduce/thread_pool.go
+++ mapreduce/thread_pool.go
@@ -1,9 +1,35 @@
 package mapreduce
 
 import (
-    "fmt"
+    //"fmt"
 )
 
-type threadPool struct {
-    
+type thrdPool struct {
+    numThrd uint32
+    numExecuting uint32
+    waitList []func()
+    newThrdCh chan func()
+    doneCh chan bool
+    innerThrdCh []chan bool
+    innerChStack []uint32
+}
+
+func newThrdPool(
+    numThrd uint32,
+    newThrdCh chan func(),
+    doneCh chan bool,
+    ) *thrdPool {
+    ret := thrdPool{
+        numThrd: numThrd,
+        numExecuting: 0,
+        newThrdCh: newThrdCh,
+        doneCh: doneCh,
+        waitList: make([]func(), numThrd*2),
+        innerThrdCh: make([]chan bool, numThrd),
+        innerChStack: make([]uint32, numThrd),
+    }
+    for i := uint32(0); i < numThrd; i++ {
+        ret.innerChStack[i] = i
+    }
+    return &ret
 }
