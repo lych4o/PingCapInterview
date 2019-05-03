b00f1caad12a45a5843b663f5a16c212a9f28fbf
lych4o <ly1ch4o@gmail.com>
2019-05-03 15:12:09 +0800

    thread_pool debug test done

--- mapreduce/thread_pool.go
+++ mapreduce/thread_pool.go
@@ -5,53 +5,57 @@ import (
     //"time"
 )
 
-type fStack struct {
-    stk []func(chan bool)
-}
-
 type emptyStackError struct {}
 func (err *emptyStackError) Error() string {
     return "Call stack.pop() when queue is empty"
 }
-func (s *fStack) size() int { return len(s.stk) }
-func (s *fStack) empty() bool { return s.size() == 0 }
-func (s *fStack) push(f func(chan bool)) { s.stk = append(s.stk, f) }
-func (s *fStack) top() func(chan bool) { return s.stk[len(s.stk)-1] }
-func (s *fStack) pop() (func(chan bool), error) {
-    if len(s.stk) == 0 { return nil, &emptyStackError{} }
-    ret := s.top()
+type FStack struct { stk []func(chan bool) }
+func NewFStack() *FStack {
+    ret := &FStack{}
+    ret.stk = make([]func(chan bool), 1000)
+    ret.stk = ret.stk[:0]
+    return ret
+}
+func (s *FStack) Size() int { return len(s.stk) }
+func (s *FStack) Empty() bool { return s.Size() == 0 }
+func (s *FStack) Push(f func(chan bool)) { s.stk = append(s.stk, f) }
+func (s *FStack) Top() func(chan bool) { return s.stk[len(s.stk)-1] }
+func (s *FStack) Pop() (func(chan bool), error) {
+    if s.Empty() { return nil, &emptyStackError{} }
+    ret := s.Top()
     s.stk = s.stk[:len(s.stk)-1]
     return ret, nil
 }
 
-type thrdPool struct {
+type ThrdPool struct {
     numThrd int
     numExecuting int
-    thrdStack fStack
+    thrdStack *FStack
     newThrdCh chan func(chan bool)
     doneCh chan bool
     innerThrdCh chan bool
 }
 
-func newThrdPool(
+func NewThrdPool(
     numThrd int,
     newThrdCh chan func(chan bool),
     doneCh chan bool,
-    ) *thrdPool {
-    ret := thrdPool{
+    ) *ThrdPool {
+    ret := ThrdPool{
         numThrd: numThrd,
         numExecuting: 0,
         newThrdCh: newThrdCh,
         doneCh: doneCh,
-        thrdStack: fStack{},
+        thrdStack: NewFStack(),
         innerThrdCh: make(chan bool),
     }
     return &ret
 }
 
-func (pool *thrdPool) runThrd() {
-    for pool.numExecuting < pool.numThrd && !pool.thrdStack.empty() {
-        f, err := pool.thrdStack.pop()
+func (pool *ThrdPool) runThrd() {
+    for pool.numExecuting < pool.numThrd && !pool.thrdStack.Empty() {
+        pool.numExecuting++
+        f, err := pool.thrdStack.Pop()
         if err != nil {
             //TODO
         }
@@ -59,7 +63,7 @@ func (pool *thrdPool) runThrd() {
     }
 }
 
-func (pool *thrdPool) run() {
+func (pool *ThrdPool) Run() {
     //Use go to call
     for thrdInput := true; thrdInput == true; {
         select {
@@ -67,13 +71,14 @@ func (pool *thrdPool) run() {
                 pool.numExecuting--
                 pool.runThrd()
             case f, ok := <-pool.newThrdCh:
-                pool.thrdStack.push(f)
+                if f != nil { pool.thrdStack.Push(f) }
                 pool.runThrd()
                 if ok == false { thrdInput = false }
         }
     }
-    for pool.numExecuting > 0 && !pool.thrdStack.empty() {
+    for pool.numExecuting > 0 || !pool.thrdStack.Empty() {
         _ = <-pool.innerThrdCh
+        pool.numExecuting--
         pool.runThrd()
     }
     pool.doneCh <- true
