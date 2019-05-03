174b34b143aed3f2581b11fd475b73694167fb4b
lych4o <ly1ch4o@gmail.com>
2019-05-03 21:27:46 +0800

    finish common_map.go, not test yet & fix compile error in common_map.go and kvbuffer.go

--- mapreduce/common_map.go
+++ mapreduce/common_map.go
@@ -2,13 +2,56 @@ package mapreduce
 
 import (
     //"fmt"
-    //"bufio"
+    "io"
+    "bufio"
+    "os"
 )
 
+//Control the max number of thread to run map task.
+var ThrdPoolSize int = 4
+
+//Max Bytes of contents to run a map task.
+var MaxMapBuffer int64 = 20*1024*1024
+
+//Get map thrd to send ThrdPool.
+func getMapThrd(
+    inFile string, //mapF input file.
+    contents string, //mapF input contents.
+    kvBufferCh chan KeyValue, //kvBuffer channel
+    mapF func(file string, contents string) []KeyValue, //map function.
+) func(chan bool) {
+    return func(ch chan bool) {
+        kvs := mapF(inFile, contents)
+        for _, kv := range kvs { kvBufferCh <- kv }
+        ch <- true
+    }
+}
+
+//Do map on single file.
 func doMap(
     inFile string,
     nReduct int,
+    kvBufferCh chan KeyValue,
     mapF func(file string, contents string) []KeyValue,
 ) {
-    
+    f, openErr := os.Open(inFile)
+    if openErr != nil { panic(openErr.Error()) }
+    defer f.Close()
+
+    newThrd, doneCh := make(chan func(chan bool)), make(chan bool)
+    pool := NewThrdPool(ThrdPoolSize, newThrd, doneCh)
+    go pool.Run()
+
+    reader := bufio.NewReader(f)
+    var contents string = ""
+    for {
+        line, _, rdErr := reader.ReadLine()
+        if rdErr == io.EOF { break; }
+        if int64(len(line) + 1 + len(contents)) > MaxMapBuffer {
+            thrd := getMapThrd(inFile, contents, kvBufferCh, mapF)
+            newThrd <- thrd
+            contents = ""
+        }
+        contents += string(line)+"\n"
+    }
 }
--- mapreduce/kvbuffer.go
+++ mapreduce/kvbuffer.go
@@ -11,7 +11,7 @@ import (
 var KvBufferSize int64 = 1024*1024*64
 
 //SpillRatio.
-var SpillRatio float32 = 0.8
+var SpillRatio float64 = 0.8
 
 //Use to define filename_id.
 var spill_round int = 0
@@ -21,7 +21,7 @@ type PKV struct {
     Key, Value string
 }
 func (p PKV) toStr() string { return p.Key + " " + p.Value }
-func NewPKV(kv KeyValue, mod int) {
+func NewPKV(kv KeyValue, mod int) PKV {
     return PKV{
         Part: MHash(kv.Key, mod),
         Key: kv.Key,
@@ -32,23 +32,24 @@ func NewPKV(kv KeyValue, mod int) {
 type PKVs []PKV
 func (a PKVs) Len() int { return len(a) }
 func (a PKVs) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a PKVs) Less(i, j int) {
+func (a PKVs) Less(i, j int) bool {
     if a[i].Part < a[j].Part { return true
     } else { return a[i].Key <= a[j].Key }
 }
 
-func spill(kvs []KeyValue, L int, R int, nReduce int, outCh chan string) {
+func spill(kvs []KeyValue, L int64, R int64, nReduce int, outCh chan string) {
     ret, file, wr := make([]string, nReduce), make([]*os.File, nReduce), make([]*bufio.Writer, nReduce)
     for i:=0; i<nReduce; i++ {
+        var openErr error
         ret[i] = strconv.Itoa(i) + "_" + strconv.Itoa(spill_round)
-        file[i], err := os.OpenFile(ret[i], os.O_WRONLY | os.O_CREATE | os.TRUNC, 0666)
-        if err != nil { panic(err.Error) }
-        wr[i] = bufio.NewWrite(file[i])
+        file[i], openErr = os.OpenFile(ret[i], os.O_WRONLY | os.O_CREATE | os.O_TRUNC, 0666)
+        if openErr != nil { panic(openErr.Error) }
+        wr[i] = bufio.NewWriter(file[i])
         defer file[i].Close()
     }
 
-    arr := make([]PKV, (R-L+KvBufferSize)%KvBufferSize)
-    for i:=0; i<arr.Len(); i++ { arr[i], L = NewPKV(kvs[L], mod), (L+1)%KvBufferSize }
+    arr := make(PKVs, (int64(R-L)+KvBufferSize)%KvBufferSize)
+    for i:=0; i<arr.Len(); i++ { arr[i], L = NewPKV(kvs[L], nReduce), int64(L+1)%KvBufferSize }
     sort.Sort(arr)
 
     for i:=0; i<arr.Len(); i++ {
@@ -56,8 +57,8 @@ func spill(kvs []KeyValue, L int, R int, nReduce int, outCh chan string) {
         if err != nil { panic(err.Error()) }
     }
 
-    for w := range wr { w.Flush() }
-    for r := range ret { outCh <- r }
+    for _, w := range wr { w.Flush() }
+    for _, r := range ret { outCh <- r }
 }
 
 func KvBuffer(
@@ -65,17 +66,17 @@ func KvBuffer(
     outCh chan string, //Output file name.
     nReduce int,
 ) {
-    L, R, size, threshold := 0, 0, 0, int64(KvBufferSize*SpillRatio)
-    buf := make([]KeyValue, kvBufferSize)
-    fileNameReceiver := make([]string, nReduce)
+    L, R, size, threshold := int64(0), int64(0), int64(0), int64(float64(KvBufferSize)*SpillRatio)
+    buf := make([]KeyValue, KvBufferSize)
 
     for kv := range inCh {
         buf[R], size, R = kv, size+1, (R+1)%KvBufferSize
-        if size >= trheshold {
+        if size >= threshold {
             spill(buf, L, R, nReduce, outCh)
             L, size = R, 0
         }
     }
+
     if L != R { spill(buf, L, R, nReduce, outCh) }
     close(outCh)
 }
